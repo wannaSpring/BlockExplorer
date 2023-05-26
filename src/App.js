f0eccf6bc0cc0d22752007ced62df2ad07045d02
wannaspring <chent1493@163.com>
2023-05-26 19:20:41 +0800

    feat(*): compelete the third  tutorial

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import { Alchemy, Network } from 'alchemy-sdk';
+import { Alchemy, Network, Utils } from 'alchemy-sdk';
 import { useEffect, useState } from 'react';
 
 import './App.css';
@@ -21,16 +21,54 @@ const alchemy = new Alchemy(settings);
 
 function App() {
   const [blockNumber, setBlockNumber] = useState();
+  const [blockInfo, setBlockInfo] = useState();
 
   useEffect(() => {
-    async function getBlockNumber() {
-      setBlockNumber(await alchemy.core.getBlockNumber());
+    try {
+      getBlockNumber().then(blockNumber => {
+        fetchBlockInfoWithTxs(blockNumber).then(info => {
+          setBlockNumber(blockNumber)
+          setBlockInfo(info);
+          console.log(info)
+        })
+      })
+    } catch (err) {
+      console.log(err)
     }
-
-    getBlockNumber();
   });
 
-  return <div className="App">Block Number: {blockNumber}</div>;
+
+  async function fetchBlockInfoWithTxs(blockNumber) {
+    return await alchemy.core.getBlockWithTransactions(blockNumber);
+  }
+
+  async function getBlockNumber() {
+    return await alchemy.core.getBlockNumber()
+  } 
+
+  async function handleTractTxs (hash) {
+    const {from, value, to} = await alchemy.core.getTransaction(hash)
+    // console.log()
+    alert(`${from} send ${Utils.formatEther(parseInt(value._hex).toString())} wei to ${to}`)
+  }
+
+  return <div className="App">
+    <div className='head'>
+      <div>baseFeePerGas: {blockInfo?.baseFeePerGas._hex}</div>
+      <div>gasLimit: {blockInfo?.gasLimit._hex}</div>
+      <div> transactions: {
+        blockInfo?.transactions.map(item => {
+          return (
+            <div className='item' style={{width: '100%', display: 'flex', justifyContent: 'space-between', cursor: 'pointer'}} onClick={() => handleTractTxs(item.hash)}>
+              <div style={{flex: 2, display: 'inline-block', overflow:'hidden', textOverflow:'ellipsis', textDecoration:'underline'}}>hash: {item?.hash}</div>
+              <div style={{flex: 1}}>gasLimit: {item?.gasLimit?._hex}</div>
+              <div style={{flex: 1}}>gasPrice: {item?.gasPrice?._hex}</div>
+            </div>
+          )
+        })
+      }</div>
+    </div>
+  </div>;
 }
 
 export default App;
